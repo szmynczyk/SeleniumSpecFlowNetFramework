82b53d528b18640dc6381be60144310ec99274bf
Marcin Szymczyk <szymczykm@outlook.com>
2020-04-15 11:45:09 +0200

    Repo initializaction

---
+++ SeleniumSpecFlowNetFramework/SeleniumSpecFlowNetFramework/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumSpecFlowNetFramework
+{
+    public class Class1
+    {
+    }
+}
